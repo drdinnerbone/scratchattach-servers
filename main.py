cbf9d6c63ffe1997487faca589ff7fa4619da2c7
Dr_Dinnerbone <156529450+drdinnerbone@users.noreply.github.com>
2024-03-07 17:49:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ except:
   os.system("pip install -U scratchattach")
   import scratchattach as scratch3
 
-session = scratch3.login("username", "password")
+session = scratch3.Session("session_id", username="username") #replace with your session_id and username
+conn = session.connect_cloud("project_id") #replace with your project id
 
-#list of commands: https://github.com/TimMcCool/scratchattach/wiki/#cloud-variables
+client = scratch3.CloudRequests(conn)
+
+@client.request
+def ping(): #called when client receives request
+    print("Ping request received")
+    return "pong" #sends back 'pong' to the Scratch project
+
+@client.event
+def on_ready():
+    print("Request handler is running")
+
+client.run() #make sure this is ALWAYS at the bottom of your Python file
